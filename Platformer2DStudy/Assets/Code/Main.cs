b0befe15fdc745b85cce3c52aa333f25593e8a65
SaintFils <gurmanoleg@yandex.ru>
2021-09-28 00:35:26 +0300

    feat: start point and animator created

---
+++ Platformer2DStudy/Assets/Code/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Code.Configs;
+using Code.Controller;
+using Code.View;
+using UnityEngine;
+
+namespace Code
+{
+    public class Main : MonoBehaviour
+    {
+        [SerializeField] private SpriteAnimationConfig playerConfig;
+        [SerializeField] private float animationSpeed = 10.0f; //Потом будет перенесено в другой класс. Тут эта информация храниться не должна
+        [SerializeField] private LevelObjectView playerView;
+
+        private SpriteAnimationController _playerAnimator;
+
+        private void Awake()
+        {
+            playerConfig = Resources.Load<SpriteAnimationConfig>("PlayerAnimConfig");
+            if (playerConfig)
+            {
+                _playerAnimator = new SpriteAnimationController(playerConfig);
+            }
+
+            if (playerView)
+            {
+                _playerAnimator.StartAnimation(playerView.spriteRenderer, AnimState.Run, true, animationSpeed);
+            }
+        }
+
+        private void Update()
+        {
+            _playerAnimator.Tick();
+        }
+    }
+}
